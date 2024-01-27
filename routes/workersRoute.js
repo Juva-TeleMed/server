132763b145504152f76cd781840d9081febf4efb
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2024-01-27 14:48:00 +0100

    worker authentication added

--- routes/workersRoute.js
+++ routes/workersRoute.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   registerWorker,
   loginWorker,
+  verifyWorkerEmail,
 } from '../controllers/workersController.js';
 
 const router = express.Router();
 
-router.post('register-worker', registerWorker);
-router.post('login-worker', loginWorker);
+router.post('/register-worker', registerWorker);
+router.get('/confirm/:workerId/:token', verifyWorkerEmail);
+router.post('/login-worker', loginWorker);
 
 export default router;
